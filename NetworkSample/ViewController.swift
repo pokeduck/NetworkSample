376a55ca2c6d66304dc786282c3c1f97948223de
pokeduck <40366083+pokeduck@users.noreply.github.com>
2021-01-26 14:51:48 +0800

    init project

---
+++ NetworkSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+// ViewController.swift
+//
+// Created by Ben for NetworkSample on 2021/1/26.
+// Copyright © 2021 Alien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
